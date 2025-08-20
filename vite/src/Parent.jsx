e3444f1bf011291139b47b9f55bb691c406d789f
nasmin-azz <nastaran.atlas@gmail.com>
2025-08-20 13:49:50 +0330

    third commit

--- vite/src/Parent.jsx
+++ vite/src/Parent.jsx
@@ -1,29 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 const Parent = () => {
-  let [posts, setPosts] = useState(null);
+  let [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
+    document.title = `Count: ${count}`;
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   .catch(error => console.error('Error fetching data:', error));
+  }, [count]);
 
-      // .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  if (posts === null) return <div>Loading.........</div>;
-
-  return (
-    <div>
-      <h1>posts list</h1>
-      <ul>
-        {posts.map((item) => (
-          <li key={item.id}>{item.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Parent;
