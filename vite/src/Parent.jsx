5c92e74b5fbc622fbceb1c8909dabdec61da04f9
nasmin-azz <nastaran.atlas@gmail.com>
2025-08-20 13:33:42 +0330

    second commit

--- vite/src/Parent.jsx
+++ vite/src/Parent.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Parent = () => {
+  let [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+
+      // .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (posts === null) return <div>Loading.........</div>;
+
   return (
     <div>
-      <h1>select color:</h1>
-      <select name="" id="" ></select>
+      <h1>posts list</h1>
+      <ul>
+        {posts.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
